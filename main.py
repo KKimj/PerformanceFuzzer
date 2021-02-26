8c0df1ba4dd75b4db2b3398772c22c581752847e
지운 김 <kkimj@hanyang.ac.kr>
2021-02-26 13:26:36 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,57 +1,75 @@
 import argparse
-
+import os.path
 class PerformanceFuzzer:
-    def __init__(self):
+    def __init__(self, dirName, fileName = "main"):
+        self.dirName = dirName
+
+        if fileName.endswith('.c'):
+            fileName = fileName[:-2]
+        self.fileName = fileName
+
+        self.filePath = "./tests/"+self.dirName+"/"+self.fileName
+
+        if os.path.isfile(self.filePath+".c") == False:
+            print("File not exist!")
+            quit()
+
+        if os.path.isfile(self.filePath+"_opt.ll") == False:
+            print("Makefile!")
+            self.Build()
+            
+
+    def Run(self):
+        pass
+
+    def Build(self):
         pass
 
     def NOP(self):
-        pass
+        return '  call void asm sideeffect "NOP;", ""()\n'
 
-    def load(self):
-        pass
+    def Insert(self):    
+        file_opt_ll = open(self.filePath+"_opt.ll", "r")
+        file_fuz_ll = open(self.filePath+"_opt_fuzzer.ll", "w")
+        
+        nop_count = 1
+        
 
-    def insert(self):
-        pass
-    
+        insert_flag = False
+        while True:
+            line = file_opt_ll.readline()
+            file_fuz_ll.write(line)
+            if not line:
+                break
 
-    pass
+            if insert_flag and len(line) < 0:
+                insert_flag = False
+
+            if insert_flag and line.startswith("}"):
+                insert_flag = False
+
+            if not insert_flag and line.startswith("; <label>") and nop_count > 0:
+                insert_flag = True
+            
+            if insert_flag and nop_count > 0:
+                file_fuz_ll.write('  call void asm sideeffect "NOP;", ""()\n')
+                nop_count -= 1
+                
+            print(line)
+
+        file_opt_ll.close()
+        file_fuz_ll.close()
 
 
 
 def main(filename_list, option_list):
-    # print('Target File : {}'.format(filename_list))
-    # print(filename_list)
-    # print('Optional : {}'.format(option_list))
-    nop_count = 1
+    if len(filename_list) == 1:
+        performanceFuzzer = PerformanceFuzzer(filename_list[0])
+    elif len(filename_list) == 2:
+        performanceFuzzer = PerformanceFuzzer(filename_list[0], filename_list[1])
 
-    file_opt_ll = open("./tests/"+filename_list[0]+"_opt.ll", "r")
-    file_fuz_ll = open("./tests/"+filename_list[0]+"_opt_fuzzer.ll", "w")
-    
-    insert_flag = False
-
-    while True:
-        line = file_opt_ll.readline()
-        file_fuz_ll.write(line)
-        if not line:
-            break
-
-        if insert_flag and len(line) < 0:
-            insert_flag = False
-
-        if insert_flag and line.startswith("}"):
-            insert_flag = False
-
-        if not insert_flag and line.startswith("; <label>") and nop_count > 0:
-            insert_flag = True
-        
-        if insert_flag and nop_count > 0:
-            file_fuz_ll.write('  call void asm sideeffect "NOP;", ""()\n')
-            nop_count -= 1
-            
-        print(line)
-
-    file_opt_ll.close()
-    file_fuz_ll.close()
+    performanceFuzzer.Insert()
+    performanceFuzzer.Run()
 
 
 
