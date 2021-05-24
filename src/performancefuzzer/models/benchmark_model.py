32ca00f0889f604e0254bad59595b0fbb36a89bf
= <kkimj@hanyang.ac.kr>
2021-05-24 11:28:39 +0900

    update Benchmark model

--- src/performancefuzzer/models/benchmark_model.py
+++ src/performancefuzzer/models/benchmark_model.py
@@ -1,2 +1,17 @@
 class BenchmarkModel:
+    def __init__(self) -> None:
+        self.task_clock = None
+        self.context_switches = None
+        self.cpu_migrations = None
+        self.cycles = None
+        self.instructions = None
+        self.branches = None
+        self.branch_misses = None
+
+        self.CPUs_utilized = None
+
+        self.user_time = None
+        self.sys_time = None
+
+    # getter time elapsed, user_time + sys_time
     pass
