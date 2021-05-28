15c7efe105b323bbbda517ecf3b7dc85e82ba758
= <kkimj@hanyang.ac.kr>
2021-05-28 19:32:00 +0900

    update for benchmark controller

--- src/performancefuzzer/models/benchmark_model.py
+++ src/performancefuzzer/models/benchmark_model.py
@@ -1,5 +1,5 @@
 class Benchmark:
-    def __init__(self) -> None:
+    def __init__(self, task_clock = None, **kwargs) -> None:
         self.task_clock = None
         self.context_switches = None
         self.cpu_migrations = None
@@ -10,8 +10,8 @@ class Benchmark:
 
         self.CPUs_utilized = None
 
+        self.time = None
         self.user_time = None
         self.sys_time = None
-
-    # getter time elapsed, user_time + sys_time
-    pass
+        
+        # self.update(kwargs)
